a9ce2e64999648039cb486746a3bce57b30c995a
victorlinard <contact@victor-linard.fr>
2022-11-29 19:16:14 +0100

    create plugin base

--- includes/functions.php
+++ includes/functions.php
@@ -1,34 +1,20 @@
 <?php
-add_action("admin_menu", "vl_p3_Add_My_Admin_Link");
+add_action("admin_menu", "ld_spotify_add_admin_menu");
 
-function vl_p3_Add_My_Admin_Link()
+function ld_spotify_add_admin_menu()
 {
     add_menu_page(
-            'Ma Belle Page Plugin3', // Title of the page
-            'Plugin 3', // Text to show on the menu link
+            'Spotify Administration', // Title of the page
+            'Spotify Administration', // Text to show on the menu link
             'manage_options', // Capability requirement to see the link
-            plugin_dir_path(__FILE__).'vl-admin-page.php'
+            plugin_dir_path(__FILE__).'spotify-administration.php'
     );
 }
 
 
 // function that runs when shortcode is called
-function vl_p3_shortcode() {
-    // Things that you want to do.
-    $message = 'Je suis là \o/ !';
-
-    // Output needs to be return
-    return $message;
+function ld_spotify_shortcode() {
+    return '<h1>Je suis là \o/ !</h1>';
 }
 
-function vl_p3_shortcode2($atts, $content = null) {
-    $default = array(
-        'link' => '#',
-    );
-    $a = shortcode_atts($default, $atts);
-    $content = do_shortcode($content);
-    return 'Voici un lien <a href="'.($a['link']).'" style="color: blue">'.$content.'</a>';
-}
-
-add_shortcode('coucou', 'vl_p3_shortcode');
-add_shortcode('coucou2', 'vl_p3_shortcode2');
+add_shortcode('ld_spotify_search_bar', 'ld_spotify_shortcode');
