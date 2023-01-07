30840174ead9572913d293d84b723f72209e668c
victorlinard <contact@victor-linard.fr>
2023-01-07 18:47:52 +0100

    exemple of iframe player

---
+++ test_iframe_api.php
@@ -0,0 +1,17 @@
+<div>
+    <script src="https://open.spotify.com/embed-podcast/iframe-api/v1" async></script>
+    <div id="embed-iframe"></div>
+
+    <script>
+        window.onSpotifyIframeApiReady = (IFrameAPI) => {
+            let element = document.getElementById('embed-iframe');
+            let options = {
+                width: '400px',
+                height: '150px',
+                uri: 'spotify:track:0SKNkEMV313mE6oYsgVCgm'
+            };
+            let callback = (EmbedController) => {};
+            IFrameAPI.createController(element, options, callback);
+        };
+    </script>
+</div
